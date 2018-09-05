639c8259d119b8c0b713f44a7da521d763be87c7
hollisallan <hollisallan@gmail.com>
2018-09-05 03:26:22 -0700

    Add files via upload

---
+++ src/main/java/com/example/demo/MapOverlayRestServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapOverlayRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapOverlayRestServiceApplication.class, args);
+	}
+}
