cb82a6d6395927481692db84dfcb9df3d47e657c
hollisallan <hollisallan@gmail.com>
2018-10-17 22:35:21 -0700

    Add files via upload

--- src/main/java/com/example/demo/service/AirportService.java
+++ src/main/java/com/example/demo/service/AirportService.java
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import com.example.demo.repository.AirportRepository;
 
 @Service
